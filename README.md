05ff68a1dba5cc0dbb803f7b96f1a548eac874b3
Vyrtsev Mikhail <reeywhaar@gmail.com>
2022-06-21 00:00:36 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# @reeywhaar/hermes-profile-transformer
+
+Simple cli wrapper over [hermes-profile-transformer](https://www.npmjs.com/package/hermes-profile-transformer) library based on it description sample code.
+
+Usage:
+
+```
+#: npx @reeywhaar/hermes-profile-transformer %input.cpuprofile% %output.json%
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+const transformer = require("hermes-profile-transformer").default;
+const { writeFileSync } = require("fs");
+const path = require("path");
+const { argv } = require("process");
+
+const hermesCpuProfilePath = argv[2];
+const outputPath = argv[3];
+const sourceMapPath = undefined;
+const sourceMapBundleFileName = undefined;
+
+if (!hermesCpuProfilePath || !outputPath) {
+  console.log(
+    "usage: npx @reeywhaar/hermes-profile-transformer %input.cpuprofile% %output.json%"
+  );
+  process.exit(0);
+}
+
+async function main() {
+  try {
+    const ip = path.resolve(hermesCpuProfilePath);
+    const op = path.resolve(outputPath);
+
+    const events = await transformer(
+      // profile path is required
+      ip,
+      // source maps are optional
+      sourceMapPath,
+      sourceMapBundleFileName
+    );
+
+    return writeFileSync(op, JSON.stringify(events, null, 2), "utf-8");
+  } catch (e) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+main();
